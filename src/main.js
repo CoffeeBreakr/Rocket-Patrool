9c075fda6339907dcc472adc439fa1fc819468be
CoffeeBreakr <jesapark@ucsc.edu>
2022-06-04 18:59:45 -0700

    fixing a major mistake

--- src/main.js
+++ src/main.js
@@ -6,7 +6,21 @@
  creating assets, creating new classes, UI changes, debugging and troubleshooting (404 err), etc 
 */
 
+let config = {
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 480,
+    scene: [Menu, Play]
+}
 
+let game = new Phaser.Game(config);
+
+//Set UI Sizes
+let borderUISize = game.config.height/15;
+let borderPadding = borderUISize/3;
+
+//Set Keyboard Vars
+let keyZ, keyX, keyLEFT, keyRIGHT;
 
 /*Point Break Down
 Novice Tier
